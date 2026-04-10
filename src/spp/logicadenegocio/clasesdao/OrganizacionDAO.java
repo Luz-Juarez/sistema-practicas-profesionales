45b82992d22009a3085f7a8b8dad409f3f6ff4b0
gomezcarballo <vicgomezcarballo@gmail.com>
2026-04-10 01:47:26 -0600

    feat: implementa metodos faltantes

--- src/spp/logicadenegocio/clasesdao/OrganizacionDAO.java
+++ src/spp/logicadenegocio/clasesdao/OrganizacionDAO.java
@@ -81,13 +81,56 @@ public class OrganizacionDAO implements IOrganizacionDAO{
 
     @Override
     public boolean eliminarOrganizacion(int idOrganizacion) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        boolean eliminacionExitosa = false;
+
+        String consultaSQL = "DELETE FROM Organizacion WHERE idOrganizacion = ?";
+
+        try (Connection conexion = ConexionBD.getConexion();
+             PreparedStatement consultaPreparada = conexion.prepareStatement(consultaSQL)) {
+
+            consultaPreparada.setInt(1, idOrganizacion);
+
+            int filasAfectadas = consultaPreparada.executeUpdate();
+
+            if (filasAfectadas > 0) {
+                eliminacionExitosa = true;
+            }
+        }catch(Exception e){
+            
+        }
+
+        return eliminacionExitosa; 
+
     }
 
     @Override
     public boolean actualizarOrganizacion(Organizacion organizacion) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+        boolean actualizacionExitosa = false;
+
+        String consultaSQL = "UPDATE Organizacion SET nombre = ?, direccion = ?, "
+                + "sector = ?, estado = ?"
+                + "WHERE idOrganizacion = ? ";
+
+        try (Connection conexion = ConexionBD.getConexion();
+             PreparedStatement consultaPreparada = conexion.prepareStatement(consultaSQL)) {
+
+            consultaPreparada.setString(1, organizacion.getNombre());
+            consultaPreparada.setString(2, organizacion.getDireccion());
+            consultaPreparada.setString(3, organizacion.getSector());
+            consultaPreparada.setBoolean(4, organizacion.getEsActivo());
+            consultaPreparada.setInt(5, organizacion.getIdOrganizacion());
+
+            int filasAfectadas = consultaPreparada.executeUpdate();
+
+            if (filasAfectadas > 0) {
+                actualizacionExitosa = true;
+            }
+        }catch(Exception e){
+            
+        }
+
+        return actualizacionExitosa;
+        }
 
     
 }
