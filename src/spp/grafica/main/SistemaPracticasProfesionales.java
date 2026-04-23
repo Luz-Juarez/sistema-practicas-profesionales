e3554452b930b3ef92f3e35f84d11b044b67dd3b
Lux <juarfernanda1830@gmail.com>
2026-04-23 17:14:39 -0600

    fix: Hash y equals para DTO profesor

--- src/spp/grafica/main/SistemaPracticasProfesionales.java
+++ src/spp/grafica/main/SistemaPracticasProfesionales.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package spp.grafica;
+package spp.grafica.main;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
