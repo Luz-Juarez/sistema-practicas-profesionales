2976363b4f25f2cc2666a949ffeb4d6579289e36
Lux <juarfernanda1830@gmail.com>
2026-04-24 00:03:22 -0600

    feat: GUI registrar practicante

---
+++ src/spp/grafica/main/SistemaPracticasProfesionales.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package spp.grafica.main;
+
+
+/**
+ *
+ * @author Luz Fernanda H J
+ */
+public class SistemaPracticasProfesionales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+    }
+
+}
