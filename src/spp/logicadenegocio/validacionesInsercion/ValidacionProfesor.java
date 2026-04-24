7d10a21b7ea5b2adbe48927b8a201e9973bcd529
gomezcarballo <vicgomezcarballo@gmail.com>
2026-04-24 09:06:39 -0600

    feat: agrega gui de profesor

---
+++ src/spp/logicadenegocio/validacionesInsercion/ValidacionProfesor.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spp.logicadenegocio.validacionesInsercion;
+
+import spp.logicadenegocio.clasesdao.ProfesorDAO;
+import spp.logicadenegocio.clasesdao.UsuarioDAO;
+import spp.logicadenegocio.clasesdto.Profesor;
+import spp.logicadenegocio.clasesdto.Usuario;
+import spp.utilerias.excepciones.OperacionesDeDaoExcepcion;
+
+/**
+ *
+ * @author gomes
+ */
+public class ValidacionProfesor {
+    
+    public void insertarProfesor(Profesor profesor){
+        
+        Usuario usuario = new Usuario();
+        
+        usuario.setNombre(profesor.getNombre());
+        usuario.setApellidos(profesor.getApellidos());
+        usuario.setContraseña("password");
+        usuario.setEsActivo(true);
+        
+        UsuarioDAO usuarioDao = new UsuarioDAO();
+        ProfesorDAO profesorDao = new ProfesorDAO();
+        
+        try{
+            int idUsuario = usuarioDao.registrarUsuario(usuario);
+            profesor.setIdUsuario(idUsuario);
+            profesorDao.registrarProfesor(profesor);
+        }catch(OperacionesDeDaoExcepcion e){
+            
+            System.out.println(e.getMessage());
+        }
+        
+    }
+}
